094098a0b3ac8129899c7e2f450a2303fb5a56de
Kamil Wnęk <kamilwnek963@gmail.com>
2020-12-15 23:17:43 +0100

    Added Net Boxed Mapper

--- OtoMotor/Controllers/HomeController.cs
+++ OtoMotor/Controllers/HomeController.cs
@@ -31,6 +31,7 @@ namespace OtoMotor.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+           
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
