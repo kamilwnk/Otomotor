f216221815d934d40535e30152efe9acb37919b3
Kamil Wnęk <kamilwnek963@gmail.com>
2020-12-22 22:18:22 +0100

    Update Dto project

---
+++ ClassLibrary1/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dto
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string EmailAdress { get; set; }
+        public bool IsActivated { get; set; }
+    }
+}
